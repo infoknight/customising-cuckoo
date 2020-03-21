03a82fc2eb819ee612064ad86ea923ccb3e82df1
infoknight <prabhu.vu3mvo@gmail.com>
2020-03-21 17:26:52 +0530

    Redefined main function

--- convertHTML2PDF.py
+++ convertHTML2PDF.py
@@ -2,8 +2,15 @@
 #pip install html2pdf
 #apt-get install wkhtmltopdf
 
+
 from html2pdf import HTMLToPDF
-fin = open("report.html","r")
-HTML = fin.readlines()
-h = HTMLToPDF(HTML[0],"report1.pdf")        #Output is saved as /tmp/report1.pdf
-h.render()
+
+def main():
+    fin = open("report.html","r")
+    HTML = fin.readlines()
+    h = HTMLToPDF(HTML[0],"report1.pdf")        #Output is saved as /tmp/report1.pdf
+    h.render()
+    return
+
+if __name__ == "__main__":
+    main()
