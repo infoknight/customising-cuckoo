013dcee0e1813f639b9090218e65427ed95cb91b
infoknight <prabhu.vu3mvo@gmail.com>
2020-03-21 17:28:37 +0530

    Redefined main function

--- convertJSON2HTML.py
+++ convertJSON2HTML.py
@@ -2,18 +2,22 @@
 from json2html import *
 import json
 
-fin = open("report.json","r")
-input = ""
-for line in fin:
-    input = input + line
+def main():
+    fin = open("report.json","r")
+    input = ""
+    for line in fin:
+        input = input + line
 
-result = json2html.convert(json = input)
+    result = json2html.convert(json = input)
 
-fout = open("report.html","w")
-fout.write("<html>")
-fout.write("<body>")
-fout.write(result)
-fout.write("</body>")
-fout.write("</html>")
-fout.close()
+    fout = open("report.html","w")
+    fout.write("<html>")
+    fout.write("<body>")
+    fout.write(result)
+    fout.write("</body>")
+    fout.write("</html>")
+    fout.close()
+    return
 
+if __name__ == "__main__":
+    main()
